aff8a8fea4fe4ba12e4328960e2067aa3fb5483f
Pascal Friedrich <pascal.friedrich@brickmakers.de>
2020-01-16 14:21:53 +0100

    Change Path of Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "msgraph-sdk-objc",
+    name: "MSGraphSDK",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "msgraph-sdk-objc",
-            targets: ["msgraph-sdk-objc"]),
+            name: "MSGraphSDK",
+            targets: ["MSGraphSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "msgraph-sdk-objc",
-            dependencies: []),
+            name: "MSGraphSDK",
+            dependencies: [],
+            path: "MSGraphClientSDK"),
         .testTarget(
             name: "msgraph-sdk-objcTests",
-            dependencies: ["msgraph-sdk-objc"]),
+            dependencies: ["MSGraphSDK"]),
     ]
 )
